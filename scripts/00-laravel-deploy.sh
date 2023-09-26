f2ba1613d204cd3ee25c541b2ff59e5b1efda7ef
Yakanaro <kipaherd@gmail.com>
2023-09-26 16:23:18 +0300

    deploy

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -12,11 +12,23 @@ php artisan config:cache
 echo "Caching routes..."
 php artisan route:cache
 
-echo "Install Node.js"
-sudo apt install nodejs npm
+# Установка Node Version Manager (nvm)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
-echo "Install NPM"
-npm install
+# Закрыть и открыть новое терминальное окно или выполнить source ~/.bashrc, чтобы активировать nvm
+source ~/.bashrc
+
+# Установка Node.js с помощью nvm
+nvm install node
+
+# Убедитесь, что Node.js установлен правильно
+node --version
+
+# Установка npm
+npm install -g npm
+
+echo "Building frontend assets with Vite..."
+npm run build
 
 echo "Building frontend assets with Vite..."
 npm run build
