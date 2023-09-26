9082f15b7f5d45bcb153599e99615f56ae280d2c
Yakanaro <kipaherd@gmail.com>
2023-09-26 23:42:55 +0300

    add is_admin

--- resources/views/diagnosic/index.blade.php
+++ resources/views/diagnosic/index.blade.php
@@ -1,12 +1,15 @@
 @extends('layouts.app')
 @section('content')
     <h1 class="text-center text-6xl pb-5">Диагностика эмоционального интеллекта</h1>
-    <h2 class="text-center text-2xl px-40">Данная диагностика основана на Тесте..
-    </h2>
     <div class="text-center pt-6">
-        <button type="button" onclick="window.location.href='{{route('diagnostic.sectionA')}}';"
-                class=" text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-3xl text-lg px-7 py-3.5 text-center mr-1 mb-1 max-w-50">
-            Начать
-        </button>
+        <div class="mb-6">
+            <label for="default-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Введите ссылку-приглашение</label>
+            <input type="text" id="default-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <button type="button"
+                    class="mt-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                    onclick="">
+                Войти
+            </button>
+        </div>
     </div>
 @endsection
