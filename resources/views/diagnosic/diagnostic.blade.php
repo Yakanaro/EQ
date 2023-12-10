ef46139b2b5fc67d5e5e821eaee775daf72d39b6
Yakanaro <kipaherd@gmail.com>
2023-12-10 20:01:14 +0300

    fix image

--- resources/views/diagnosic/diagnostic.blade.php
+++ resources/views/diagnosic/diagnostic.blade.php
@@ -40,7 +40,7 @@
                 </tbody>
             </table>
 
-            <button type="submit">Отправить</button>
+            <button type="submit" class="mt-5 mb-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Сохранить</button>
         </div>
     </form>
 @endsection
